9bd833c5f470b29b48c522a638d65f2b4319e2e8
Da Lu <light_1986@msn.com>
2025-12-28 13:05:47 +0800

    update

--- walnut_pair_backend/src/business_layers/walnut_bl.py
+++ walnut_pair_backend/src/business_layers/walnut_bl.py
@@ -5,7 +5,7 @@ from src.domain_layers.services.embedding_service import (
     IImageEmbeddingService,
 )
 from src.common.interfaces import IAppConfig, IDatabaseConnection
-from src.data_access_layers.db_readers import IWalnutImageEmbeddingReader
+from src.data_access_layers.db_readers import IWalnutImageEmbeddingReader, IWalnutReader
 
 
 class IWalnutBL(ABC):
@@ -22,11 +22,13 @@ class WalnutBL(IWalnutBL):
         app_config: IAppConfig,
         db_connection: IDatabaseConnection,
         walnut_image_embedding_reader: IWalnutImageEmbeddingReader,
+        walnut_reader: IWalnutReader,
     ) -> None:  
         self.image_embedding_service = image_embedding_service
         self.app_config = app_config
         self.db_connection = db_connection
         self.walnut_image_embedding_reader = walnut_image_embedding_reader
+        self.walnut_reader = walnut_reader
     
     def generate_embeddings(self) -> None:
         print(self.app_config.image_root)
@@ -35,4 +37,6 @@ class WalnutBL(IWalnutBL):
         embedding = self.image_embedding_service.generate(image)
         test = self.walnut_image_embedding_reader.get_by_model_name("resnet50-imagenet")
         print(f"Found {len(test)} embeddings")
+        walnuts = self.walnut_reader.get_all()
+        print(f"Found {len(walnuts)} walnuts")
 
